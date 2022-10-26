8b13b27cc50e6023ab05c59b295ae6fa9f3342da
mbugua-richards <ricmwathih81@gmail.com>
2022-10-26 06:14:47 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
   return (
-	<h1 className='underline text-3xl'>App</h1>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3
+						hover:drop-shadow-xl
+						hover:bg-light-gray text-white"
+                style={{ background: "purple", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed  sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">Sidebar w-0</div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          ></div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
